7d0f1be5554c8eb0340678529dba41940ea968fe
mbataprecious <mbataprecious9@gmail.com>
2023-04-27 04:35:07 +0100

    nora hair deployCLOUDINARY_SECRET

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,5 +3,5 @@ import db from "../../utils/db";
 export default function handler(req, res) {
   db.connectDb();
   db.disconnectDb();
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "i am live" });
 }
